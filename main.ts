eecc2b707c3cfa415ddd840679980eedac4f5f62
Adam Hao <haozg44@outlook.com>
2022-10-03 01:35:59 +0800

    functionatily ready for macOS

--- main.ts
+++ main.ts
@@ -1,137 +1,123 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+/* eslint-disable prefer-const */
+import {
+	// App,
+	Editor,
+	Plugin,
+	normalizePath,
+} from "obsidian";
 
-// Remember to rename these classes and interfaces!
+import * as path from "path";
+import * as jetpack from "fs-jetpack";
+import { clipboard } from "electron";
+// import { readFilePaths } from "electron-clipboard-ex";
 
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+export default class MarkdownMedia extends Plugin {
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+			id: "insert-media-markdown",
+			name: "Insert media markdown",
+			editorCallback: (editor: Editor) => {
+				let sourceFileDataMap = new Map();
+				let sourceFilePaths = this.sourceFilePaths();
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+				// sourceFilePaths = readFilePaths();
+				// console.log(sourceFilePaths);
+
+				for (let sourceFilePath of sourceFilePaths) {
+					sourceFileDataMap.set(
+						sourceFilePath,
+						path.parse(sourceFilePath)
+					);
 				}
-			}
+				// copy file, and insert markdown syntax at cursor position
+				for (let [key, value] of sourceFileDataMap) {
+					jetpack.copy(
+						key,
+						normalizePath(
+							`${this.destinationPath()}/${value.base}`
+						),
+						{
+							overwrite: (srcInspectData, destInspectData) => {
+								return (
+									srcInspectData.modifyTime >
+									destInspectData.modifyTime
+								);
+							},
+						}
+					);
+					if (this.isImageURL(value.ext)) {
+						editor.replaceRange(
+							`![${value.name}](${value.base})\n`,
+							editor.getCursor()
+						);
+					} else {
+						editor.replaceRange(
+							`[${value.name}](${value.base})\n`,
+							editor.getCursor()
+						);
+					}
+				}
+			},
 		});
+	}
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+	destinationPath() {
+		let vaultPath = app.vault.adapter.getBasePath();
+		let activeFileParentFolder = app.workspace.getActiveFile()?.parent.path;
+		let destinationPath = path.format({
+			dir: vaultPath,
+			base: `${activeFileParentFolder}/media`,
 		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		return normalizePath(destinationPath);
 	}
 
-	onunload() {
+	sourceFilePaths() {
+		// read properties of clipboard files
+		// https://github.com/njzydark/Aragorn/blob/afe4a60972b4255dd417480ca6aca2af1fd8e637/packages/aragorn-app-main/src/uploaderManager.ts#L88
 
+		let filePath;
+		if (process.platform === "darwin") {
+			// https://github.com/electron/electron/issues/9035#issuecomment-359554116
+			filePath =
+				clipboard
+					.read("NSFilenamesPboardType")
+					.match(/<string>.*<\/string>/g)
+					?.map((item) => item.replace(/<string>|<\/string>/g, "")) ||
+				[];
+		} else {
+			// currently not working in win
+			// https://github.com/electron/electron/issues/9035#issuecomment-536135202
+			// https://docs.microsoft.com/en-us/windows/win32/shell/clipboard#cf_hdrop
+			// https://www.codeproject.com/Reference/1091137/Windows-Clipboard-Formats
+			// console.log(clipboard.read("CF_HDROP"));
+			// let rawFilePathStr = clipboard.read("CF_HDROP") || "";
+			// let formatFilePathStr = [...rawFilePathStr]
+			// 	.filter((_, index) => rawFilePathStr.charCodeAt(index) !== 0)
+			// 	.join("")
+			// 	.replace(/\\/g, "\\");
+			// const drivePrefix = formatFilePathStr.match(/[a-zA-Z]:\\/);
+			// if (drivePrefix) {
+			// 	const drivePrefixIndex = formatFilePathStr.indexOf(
+			// 		drivePrefix[0]
+			// 	);
+			// 	if (drivePrefixIndex !== 0) {
+			// 		formatFilePathStr =
+			// 			formatFilePathStr.substring(drivePrefixIndex);
+			// 	}
+			// 	filePath = formatFilePathStr
+			// 		.split(drivePrefix[0])
+			// 		.filter((item) => item)
+			// 		.map((item) => drivePrefix + item);
+			// }
+		}
+		return filePath;
 	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+	isImageURL(string: string) {
+		return (
+			string.match(
+				/\.(jpeg|jpg|gif|png|tif|tiff|bmp|eps|raw|apng|avif|jfif|pjpeg|pjp|svg|webp)$/
+			) != null
+		);
 	}
 }
